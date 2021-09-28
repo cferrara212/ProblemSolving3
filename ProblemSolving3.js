14f0a07ea803166013f221ed29b5c442af731d96
unknown <cferrara212@gmail.com>
2021-09-28 14:36:01 -0600

    updating with code block that will check if an array is a number sequence

--- ProblemSolving3.js
+++ ProblemSolving3.js
@@ -54,51 +54,88 @@ function findIndices(numberArray,numberCanAddTo)
               ///
                     /// writing variables for palindrom
 
-                    let wordToCheck= prompt("please enter a word to check if it is a palindrome!");
-                    let trimmedWordToCheck = wordToCheck.trim();
-                    let wordReversed = reverseThisWord(trimmedWordToCheck);
-                    let isPalindrome = checkIfPalindrome(trimmedWordToCheck, wordReversed);
+                    // let wordToCheck= prompt("please enter a word to check if it is a palindrome!");
+                    // let trimmedWordToCheck = wordToCheck.trim();
+                    // let wordReversed = reverseThisWord(trimmedWordToCheck);
+                    // let isPalindrome = checkIfPalindrome(trimmedWordToCheck, wordReversed);
 
 
                     ////
                     //// adding function to reverse word
                     
-                function reverseThisWord(wordToBeReversed)
-                 {   
-                     let wordNowReversed ="";
-                    for (let i = wordToBeReversed.length-1; i>=0; i--)
-                    {
-                       wordNowReversed += wordToBeReversed[i];
-                    }
-                    return wordNowReversed;
-                }
-                    console.log(wordReversed);
-                    console.log(isPalindrome);
+                // function reverseThisWord(wordToBeReversed)
+                //  {   
+                //      let wordNowReversed ="";
+                //     for (let i = wordToBeReversed.length-1; i>=0; i--)
+                //     {
+                //        wordNowReversed += wordToBeReversed[i];
+                //     }
+                //     return wordNowReversed;
+                // }
+                //     console.log(wordReversed);
+                //     console.log(isPalindrome);
 
 
                     ////
                     //// adding bool function to cross reference the word to the reversed word and return true or false for palindrome.
 
 
-                    function checkIfPalindrome(wordToBeChecked, reversedWordToCheck)
+                    // function checkIfPalindrome(wordToBeChecked, reversedWordToCheck)
                     
-                    {
-                        let palindrome;
-                      for (let i=0; i<wordToBeChecked.length; i++)
-                        {
+                    // {
+                    //     let palindrome;
+                    //   for (let i=0; i<wordToBeChecked.length; i++)
+                    //     {
                             
                              
-                                 if (reversedWordToCheck[i] === wordToBeChecked[i])
-                                  {
-                                      palindrome =true
+                    //              if (reversedWordToCheck[i] === wordToBeChecked[i])
+                    //               {
+                    //                   palindrome =true
                                       
-                                  }
-                                  else
-                                  {
-                                      palindrome = false
-                                      break;
-                                  }
+                    //               }
+                    //               else
+                    //               {
+                    //                   palindrome = false
+                    //                   break;
+                    //               }
                              
+                    //     }
+                    //     return palindrome
+                    // }
+
+
+
+
+
+//////
+//////
+//////3.Given a list of integers, return a bool that represents whether or not all integers in the list can form a sequence of incrementing integers
+//////a.Use case: 
+//////i.{5, 7, 3, 8, 6}  false (no 4 to complete the sequence)
+//////ii.{17, 15, 20, 19, 21, 16, 18}  true
+
+
+
+                    
+                    let numberSequence= [7,6,4,3,1,2];
+                    let isNumberSequencePossible= checkThisSequence(numberSequence);
+                    console.log(isNumberSequencePossible);
+
+
+                    function checkThisSequence(yourNumberSequence)
+                    {
+                        yourNumberSequence.sort()
+                        let isItPossible=true;
+                        if (isItPossible===true)
+                        {
+                        for(let i=1; i<yourNumberSequence.length; i++)
+                        {
+                             if(yourNumberSequence[i]-1 !== yourNumberSequence[i-1])
+                             {
+                                 isItPossible=false;
+                             }
                         }
-                        return palindrome
+                        }
+                        return isItPossible;
                     }
+
